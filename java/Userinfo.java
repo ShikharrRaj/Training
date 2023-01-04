bbe6377cae88e8d45f30ac998603a560aec9bdd1
Shikhar raj <shikhar132020@gmail.com>
2023-01-04 14:59:41 +0530

    Userinfo.java

---
+++ java/Userinfo.java
@@ -0,0 +1,46 @@
+package java;
+
+import java.util.Scanner;
+
+public class Userinfo {
+    public String name;
+    private int userid;
+    public String companyname;
+    public String place;
+    protected int age;
+    private String position;
+   
+    
+    
+    public void createuser(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter details");
+        System.out.println("Name");
+        name = scan.next();
+        System.out.println("userid");
+        userid= scan.nextInt();
+        System.out.println("companyname");
+        companyname=scan.next();
+        System.out.println("age");
+        age=scan.nextInt();
+        System.out.println("position");
+        position=scan.next();
+        System.out.println("place");
+        place=scan.next();
+
+    }
+    void displayUserinfo(){
+        System.out.println("Name:" +name);
+        System.out.println("userid" +userid);
+        System.out.println("companyname:" +companyname);
+        System.out.println("place" +place);
+        System.out.println("age" +age);
+        System.out.println("position:" +position);
+    }
+    public static void main(String[] args) {
+        Userinfo obj1 = new Userinfo();
+        obj1.createuser();
+        obj1.displayUserinfo();
+
+    }
+}
