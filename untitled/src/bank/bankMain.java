8b1dfedf3eece0dc7bd76c36283d1069b20fbcd0
Shikhar raj <shikhar132020@gmail.com>
2023-01-05 16:52:51 +0530

    bank

---
+++ untitled/src/bank/bankMain.java
@@ -0,0 +1,30 @@
+
+
+package bank;
+public class bankMain {
+    String bannkname;
+    int bankid;
+    long bankphno;
+    private int banktotalfund;
+    private float bankInt;
+
+    public bankMain(String bankname, int banktotalfund, float bankInt, long banktotalfound, float bankInt) {
+        this.banktotalfund = banktotalfund;
+        this.bankInt = bankInt;
+    }
+
+    public void checkLoan(int loanAmount){
+        if(loanAmount < banktotalfund){
+            System.out.println("loan approved");
+            totalIntr(branchIntr);
+        }
+    }
+
+    void totalIntr(float branchIntr){
+        System.err.println("Your total intr will be:" +(branchIntr + this.bankInt));
+
+    }
+
+
+
+}
---
+++ untitled/src/bank/bankbranch.java
@@ -0,0 +1,36 @@
+
+package  bank;
+
+import java.util.Scanner;
+
+public class bankbranch extends bankMain{
+    String branchname;
+    int branchid;
+    long branchphno;
+    private int branchtotalfund;
+    private float branchIntr;
+
+
+    public bankbranch(String bankname, int banktotalfund, float bankInt, long banktotalfound, float bankInt2,
+                      String branchname, int branchid, long branchphno, int branchtotalfund, float branchIntr) {
+        super(bankname, banktotalfund, bankInt, banktotalfound, bankInt2);
+        this.branchname = branchname;
+        this.branchid = branchid;
+        this.branchphno = branchphno;
+        this.branchtotalfund = branchtotalfund;
+        this.branchIntr = branchIntr;
+    }
+
+
+    public static void main(String args[]){
+        bankbranch Axis= new bankbranch("axis", 2323, 032626221, 10000000, 5f, "mumbai", 5555, 85262326, 10000, 4f);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the loan amount");
+        int loan=sc.nextInt();
+        Axis.checkLoan(loan, Axis.branchIntr);
+
+    }
+
+
+
+}
